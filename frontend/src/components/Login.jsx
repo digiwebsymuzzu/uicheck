400e772e73685b069676c96bcd783d1883bf4e90
Digital Websynation LLP <muzammilmegatask@gmail.com>
2025-10-06 16:03:05 +0530

    Initial commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
       return handleError("name,email,password field are required");
     }
     try {
-      const url = "https://udemandme.cloud/auth/login";
+      const url = "https://udemandme.cloud/api/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
