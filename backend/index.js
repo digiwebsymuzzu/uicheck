e3f0b229e8dfc81f6614cbc463117bf961bfd239
Digital Websynation LLP <muzammilmegatask@gmail.com>
2025-10-06 16:07:07 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/auth", AuthRouter);
+app.use("/api/auth", AuthRouter);
 app.use("/api/categories", categoryRoutes);
 app.use("/api", attributeRoutes);
 app.use("/api/quotation", quotationRoutes);
